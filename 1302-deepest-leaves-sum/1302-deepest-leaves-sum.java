7dc95d5d1d86cde8c3dc8657b08fd40d3cd2c5af
Heekyeong Park <101381901+gmlrude@users.noreply.github.com>
2024-01-19 04:16:46 +0900

    Time: 1 ms (100.00%), Space: 46.8 MB (10.21%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int deepLevel = 0;
+    int sum = 0;
+    public int deepestLeavesSum(TreeNode root) {
+        dfs(root, 0);
+
+        return sum;
+    }
+    void dfs(TreeNode root, int level) {
+        if(root == null) return;
+
+        if(root.left == null && root.right == null) {
+            if (level > deepLevel) {    // 더 깊은 경우(갱신)
+                deepLevel = level;
+                sum = root.val; // 더 깊은 레벨을 찾았으므로 sum을 현재 노드 값으로 초기화
+            } 
+            else if (level == deepLevel) {
+                sum += root.val;
+            }
+        }
+
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+}
